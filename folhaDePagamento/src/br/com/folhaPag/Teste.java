d4eacb954a945036a586e6aa648c926172d5f745
Pedro Cancella Oliveira <pedrocancella94@gmail.com>
2024-09-16 11:40:49 -0300

    Correção calculo

--- folhaDePagamento/src/br/com/folhaPag/Teste.java
+++ folhaDePagamento/src/br/com/folhaPag/Teste.java
@@ -15,28 +15,25 @@ public class Teste {
 		List<Dependente> dependentes = new ArrayList<>();
 		List<Funcionario> funcionarios = new ArrayList<>();
 
-		Funcionario funcionario = new Funcionario("Carlos Silva", "111.222.333-44", LocalDate.of(1985, 6, 15), 8500.00,
-				dependentes);
+		Funcionario funcionario = new Funcionario("Carlos Silva", "111.222.333-44", LocalDate.of(1985, 6, 15), 6500.00, dependentes);
+		
 		funcionarios.add(funcionario);
 
-		Dependente dependente1 = new Dependente("João", "123.456.789-01", LocalDate.of(2015, 5, 10), Parentesco.FILHO,
-				funcionario);
-
-		Dependente dependente2 = new Dependente("Maria", "987.654.321-09", LocalDate.of(2012, 3, 22),
-				Parentesco.SOBRINHO, funcionario);
+		Dependente dependente1 = new Dependente("João", "123.456.789-01", LocalDate.of(2015, 5, 10), Parentesco.FILHO, funcionario);
+		Dependente dependente2 = new Dependente("Maria", "987.654.321-09", LocalDate.of(2012, 3, 22), Parentesco.SOBRINHO, funcionario);
 
 		dependentes.add(dependente1);
 		dependentes.add(dependente2);
 
 		// System.out.println("Funcionário criado: " + funcionario.getNome());
 
-		funcionario.setDependente(dependentes);
 
 		System.out.println("Salário Bruto: " + funcionario.getSalarioBruto());
 		System.out.println("Desconto INSS: " + funcionario.calculoInss());
 		System.out.println("Desconto IR: " + funcionario.calculoIR());
 
 		System.out.println("Dependentes do funcionário: " + funcionario.getDependente());
+		
 
 		funcionario.somaDependente();
 	}
