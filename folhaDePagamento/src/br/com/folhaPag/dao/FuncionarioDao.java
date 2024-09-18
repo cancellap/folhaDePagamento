9c09d932f6a07c12ae56374ee040d8cb090e1be1
Pedro Cancella Oliveira <pedrocancella94@gmail.com>
2024-09-18 19:16:45 -0300

    dataBase

--- folhaDePagamento/src/br/com/folhaPag/dao/FuncionarioDao.java
+++ folhaDePagamento/src/br/com/folhaPag/dao/FuncionarioDao.java
@@ -19,20 +19,21 @@ public class FuncionarioDao {
 	}
 
 	public void inserir(Funcionario funcionario) {
-		String sqlFuncionario = "INSERT INTO funcionario(nome, cpf, datanascimento, salarioBruto) VALUES (?, ?, ?, ?)" +
-	"on conflict (cpf) do update set nome= excluded.nome , datanascimento= excluded.datanascimento, salarioBruto= excluded.salarioBruto";
-		
-		String sqlDependente = "INSERT INTO dependente(cpf, nome, datanascimento, id_funcionario) VALUES (?, ?, ?, ?)" + 
-		 "on conflict (cpf) do update set nome= excluded.nome , datanascimento= excluded.datanascimento";
-		
+		String sqlFuncionario = "INSERT INTO funcionario(nome, cpf, datanascimento, salarioBruto) VALUES (?, ?, ?, ?)"
+				+ "on conflict (cpf) do update set nome= excluded.nome , datanascimento= excluded.datanascimento, salarioBruto= excluded.salarioBruto";
+
+		String sqlDependente = "INSERT INTO dependente(cpf, nome, datanascimento, id_funcionario) VALUES (?, ?, ?, ?)"
+				+ "on conflict (cpf) do update set nome= excluded.nome , datanascimento= excluded.datanascimento";
+
 		try {
-			PreparedStatement stmtFunc = connection.prepareStatement(sqlFuncionario, PreparedStatement.RETURN_GENERATED_KEYS);
+			PreparedStatement stmtFunc = connection.prepareStatement(sqlFuncionario,
+					PreparedStatement.RETURN_GENERATED_KEYS);
 			stmtFunc.setString(1, funcionario.getNome());
 			stmtFunc.setString(2, funcionario.getCpf());
 			stmtFunc.setObject(3, funcionario.getDataNascimento());
 			stmtFunc.setDouble(4, funcionario.getSalarioBruto());
 			stmtFunc.execute();
-			//stmtFunc.close();
+			// stmtFunc.close();
 			// esse getGeneratekeys pega chave primariaa
 			ResultSet pk = stmtFunc.getGeneratedKeys();
 
@@ -40,24 +41,24 @@ public class FuncionarioDao {
 				int funcionarioId = pk.getInt(1);
 
 				List<Dependente> dependentes = funcionario.getDependentes();
-	            if (dependentes != null) {
-	                for (Dependente dependente : dependentes) {
-	                	PreparedStatement stmtDependente = connection.prepareStatement(sqlDependente);
-	                	stmtDependente.setString(1, dependente.getCpf());
-	                    stmtDependente.setString(2, dependente.getNome());
-	                    stmtDependente.setObject(3, dependente.getDataNascimento());
-	                    stmtDependente.setInt(4, funcionarioId);
-	                    stmtDependente.execute();
-	                    stmtDependente.close();
-	                }
-	            }
+				if (dependentes != null) {
+					for (Dependente dependente : dependentes) {
+						PreparedStatement stmtDependente = connection.prepareStatement(sqlDependente);
+						stmtDependente.setString(1, dependente.getCpf());
+						stmtDependente.setString(2, dependente.getNome());
+						stmtDependente.setObject(3, dependente.getDataNascimento());
+						stmtDependente.setInt(4, funcionarioId);
+						stmtDependente.execute();
+						stmtDependente.close();
+					}
+				}
 
 			}
-			//connection.close();
+			// connection.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
